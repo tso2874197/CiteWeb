3993f718ff7de71e67b3a39f0a61eb504677ad8b
Howard <xbgskg@yahoo.com.tw>
2021-04-29 20:58:20 +0800

    add jqueryUI and pagelist

--- CitcWeb/App_Start/BundleConfig.cs
+++ CitcWeb/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace CitcWeb
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryUI").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -25,6 +28,8 @@ namespace CitcWeb
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
+                      "~/Content/themes/base/all.css",
+                      "~/Content/PagedList.css",
                       "~/Content/font-awesome.css"));
         }
     }
