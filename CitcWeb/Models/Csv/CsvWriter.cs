f29531ea4f55781018f3fa27b2b04df0134f5e3f
Howard <xbgskg@yahoo.com.tw>
2021-04-30 09:28:01 +0800

    csv

---
+++ CitcWeb/Models/Csv/CsvWriter.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CitcWeb.Models.Csv
+{
+	public class CsvWriter<T> where T : CsvableBase
+    {
+        public void Write(IEnumerable<T> objects, string destination)
+        {
+            var objs = objects as IList<T> ?? objects.ToList();
+            if (objs.Any())
+            {
+                using (var sw = new StreamWriter(destination))
+                {
+                    foreach (var obj in objs)
+                    {
+                        sw.WriteLine(obj.ToCsv());
+                    }
+                }
+            }
+        }
+
+        public void Write(IEnumerable<T> objects, string destination,
+            string[] propertyNames, bool isIgnore)
+        {
+            var objs = objects as IList<T> ?? objects.ToList();
+            if (objs.Any())
+            {
+                using (var sw = new StreamWriter(destination))
+                {
+                    foreach (var obj in objs)
+                    {
+                        sw.WriteLine(obj.ToCsv(propertyNames, isIgnore));
+                    }
+                }
+            }
+        }
+
+        public void Write(IEnumerable<T> objects, string destination,
+            int[] propertyIndexes, bool isIgnore)
+        {
+            var objs = objects as IList<T> ?? objects.ToList();
+            if (objs.Any())
+            {
+                using (var sw = new StreamWriter(destination))
+                {
+                    foreach (var obj in objs)
+                    {
+                        sw.WriteLine(obj.ToCsv(propertyIndexes, isIgnore));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CitcWeb/Models/Csv/CsvableBase.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace CitcWeb.Models.Csv
+{
+    public abstract class CsvableBase
+    {
+        private string PreProcess(string input)
+        {
+            input = input.Replace('ı', 'i')
+                .Replace('ç', 'c')
+                .Replace('ö', 'o')
+                .Replace('ş', 's')
+                .Replace('ü', 'u')
+                .Replace('ğ', 'g')
+                .Replace('İ', 'I')
+                .Replace('Ç', 'C')
+                .Replace('Ö', 'O')
+                .Replace('Ş', 'S')
+                .Replace('Ü', 'U')
+                .Replace('Ğ', 'G')
+                .Replace("\"", "\"\"")
+                    .Trim();
+            if (input.Contains(","))
+            {
+                input = "\"" + input + "\"";
+            }
+            return input;
+        }
+        public virtual string ToCsv()
+        {
+            string output = "";
+
+            var properties = GetType().GetProperties();
+
+            for (var i = 0; i < properties.Length; i++)
+            {
+                if (properties[i].PropertyType.IsSubclassOf(typeof(CsvableBase)))
+                {
+                    var m = properties[i].PropertyType
+                            .GetMethod("ToCsv", new Type[0]);
+                    output += m.Invoke(properties[i].GetValue(this),
+                                        new object[0]);
+                }
+                else
+                {
+                    output += PreProcess(properties[i]
+                                        .GetValue(this).ToString());
+                }
+                if (i != properties.Length - 1)
+                {
+                    output += ",";
+                }
+            }
+
+            return output;
+        }
+
+        public virtual string ToCsv(string[] propertyNames, bool isIgnore)
+        {
+            string output = "";
+            bool isFirstPropertyWritten = false;
+
+
+            var properties = GetType().GetProperties();
+
+            for (var i = 0; i < properties.Length; i++)
+            {
+                if (isIgnore)
+                {
+                    if (!propertyNames.Contains(properties[i].Name))
+                    {
+                        if (isFirstPropertyWritten)
+                        {
+                            output += ",";
+                        }
+
+                        if (properties[i].PropertyType
+                            .IsSubclassOf(typeof(CsvableBase)))
+                        {
+                            var m = properties[i].PropertyType
+                            .GetMethod("ToCsv", new Type[0]);
+                            output += m.Invoke(properties[i].GetValue(this),
+                                                new object[0]);
+                        }
+                        else
+                        {
+                            output += PreProcess(properties[i]
+                                        .GetValue(this).ToString());
+                        }
+
+                        if (!isFirstPropertyWritten)
+                        {
+                            isFirstPropertyWritten = true;
+                        }
+                    }
+                }
+                else
+                {
+                    if (propertyNames.Contains(properties[i].Name))
+                    {
+                        if (isFirstPropertyWritten)
+                        {
+                            output += ",";
+                        }
+
+                        if (properties[i].PropertyType
+                        .IsSubclassOf(typeof(CsvableBase)))
+                        {
+                            var m = properties[i].PropertyType
+                                    .GetMethod("ToCsv", new Type[0]);
+                            output += m.Invoke(properties[i].GetValue(this),
+                                                new object[0]);
+                        }
+                        else
+                        {
+                            output += PreProcess(properties[i]
+                                        .GetValue(this).ToString());
+                        }
+
+                        if (!isFirstPropertyWritten)
+                        {
+                            isFirstPropertyWritten = true;
+                        }
+                    }
+                }
+            }
+
+            return output;
+        }
+
+        public virtual string ToCsv(int[] propertyIndexes, bool isIgnore)
+        {
+            string output = "";
+
+            bool isFirstPropertyWritten = false;
+
+            var properties = GetType().GetProperties();
+
+            for (var i = 0; i < properties.Length; i++)
+            {
+                if (isIgnore)
+                {
+                    if (!propertyIndexes.Contains(i))
+                    {
+                        if (isFirstPropertyWritten)
+                        {
+                            output += ",";
+                        }
+
+                        if (properties[i].PropertyType
+                            .IsSubclassOf(typeof(CsvableBase)))
+                        {
+                            var m = properties[i].PropertyType
+                                    .GetMethod("ToCsv", new Type[0]);
+                            output += m.Invoke(properties[i].GetValue(this),
+                                                new object[0]);
+                        }
+                        else
+                        {
+                            output += PreProcess(properties[i]
+                                        .GetValue(this).ToString());
+                        }
+
+                        if (!isFirstPropertyWritten)
+                        {
+                            isFirstPropertyWritten = true;
+                        }
+                    }
+                }
+                else
+                {
+                    if (propertyIndexes.Contains(i))
+                    {
+                        if (isFirstPropertyWritten)
+                        {
+                            output += ",";
+                        }
+
+                        if (properties[i].PropertyType
+                            .IsSubclassOf(typeof(CsvableBase)))
+                        {
+                            var m = properties[i].PropertyType
+                                    .GetMethod("ToCsv", new Type[0]);
+                            output += m.Invoke(properties[i].GetValue(this),
+                                                new object[0]);
+                        }
+                        else
+                        {
+                            output += PreProcess(properties[i]
+                                        .GetValue(this).ToString());
+                        }
+
+                        if (!isFirstPropertyWritten)
+                        {
+                            isFirstPropertyWritten = true;
+                        }
+                    }
+                }
+
+            }
+
+            return output;
+        }
+        public virtual void AssignValuesFromCsv(string[] propertyValues)
+        {
+            var properties = GetType().GetProperties();
+            for (var i = 0; i < properties.Length; i++)
+            {
+                if (properties[i].PropertyType
+                    .IsSubclassOf(typeof(CsvableBase)))
+                {
+                    var instance = Activator.CreateInstance(properties[i].PropertyType);
+                    var instanceProperties = instance.GetType().GetProperties();
+                    var propertyList = new List<string>();
+
+                    for (var j = 0; j < instanceProperties.Length; j++)
+                    {
+                        propertyList.Add(propertyValues[i + j]);
+                    }
+                    var m = instance.GetType().GetMethod("AssignValuesFromCsv", new Type[] { typeof(string[]) });
+                    m.Invoke(instance, new object[] { propertyList.ToArray() });
+                    properties[i].SetValue(this, instance);
+
+                    i += instanceProperties.Length;
+                }
+                else
+                {
+                    var type = properties[i].PropertyType.Name;
+                    switch (type)
+                    {
+                        case "Int32":
+                            properties[i].SetValue(this,
+                                int.Parse(propertyValues[i]));
+                            break;
+                        default:
+                            properties[i].SetValue(this, propertyValues[i]);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
